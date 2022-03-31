b5a16cacdc92769a50449643b2ad484f4d10a689
manasvivaghasiya <96326072+manasvivaghasiya@users.noreply.github.com>
2022-03-31 18:44:37 +0530

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ProductDescriptionComponent } from './product-description/product-description.component';
+import { CategoryWomenComponent } from './category-women/category-women.component';
+import { HomeComponent } from './home/home.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    CheckoutComponent,
+    ProductDescriptionComponent,
+    CategoryWomenComponent,
+    HomeComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/category-women/category-women.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-category-women',
+  templateUrl: './category-women.component.html',
+  styleUrls: ['./category-women.component.css']
+})
+export class CategoryWomenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
