6d2270abf0de579649145f5f06afef2b4fb32a1a
manasvivaghasiya <96326072+manasvivaghasiya@users.noreply.github.com>
2022-04-01 13:02:38 +0530

    search

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CartComponent } from './cart/cart.component';
+import { CategoryWomenComponent } from './category-women/category-women.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { HomeComponent } from './home/home.component';
+import { ProductDescriptionComponent } from './product-description/product-description.component';
+import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
-  {path:'cart',component:CartComponent}
+  {path:'cart',component:CartComponent},
+  {path:'category',component:CategoryWomenComponent},
+  {path:'checkout',component:CheckoutComponent},
+  {path:'product',component:ProductDescriptionComponent},
+  {path:'home',component:HomeComponent},
+  {path:'search',component:SearchComponent}
+
 ];
 
 @NgModule({
